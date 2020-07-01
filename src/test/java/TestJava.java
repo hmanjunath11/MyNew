4cef3c1af39289f9603b66c13e3c827d5b7fec42
hmanjunath11 <hmanjunath11@gmail.com>
2020-07-02 00:13:19 +0530

    Test methods are added

--- src/test/java/TestJava.java
+++ src/test/java/TestJava.java
@@ -3,8 +3,14 @@ import org.testng.annotations.Test;
 public class TestJava {
 
 	@Test
-	public void method()
+	public void method1()
 	{
 		System.out.println("Hi..Welcome to GitHub -> Jenkin");
+	}
+	
+	@Test
+	public void method2()
+	{
+		System.out.println("started TestNG Project");
 	}
 }

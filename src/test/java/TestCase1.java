171a916650b7b64cce47258348700e4751531d70
hmanjunath11 <hmanjunath11@gmail.com>
2020-07-03 00:36:05 +0530

    New TestNg Files are added

---
+++ src/test/java/TestCase1.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	@Test
+	public void testMethod1()
+	{
+		System.out.println("TestCase1 -> Method1");
+	}
+	
+	@Test
+	public void testMethod2()
+	{
+		System.out.println("TestCase1 -> Method2");
+	}
+}
---
+++ src/test/java/TestCase2.java
@@ -0,0 +1,15 @@
+
+
+
+import org.testng.annotations.Test;
+
+
+public class TestCase2 {
+
+	@Test (enabled=true)
+	public void testMethod1() {
+	  
+	      System.out.println("TestCase2 -> Method1");
+	   }
+
+}

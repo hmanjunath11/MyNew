9bff0c26c9c8f051e4f59193c12e5ba6e2d2431f
hmanjunath11 <hmanjunath11@gmail.com>
2020-07-03 09:41:34 +0530

    TestNG examples

---
+++ src/test/java/GroupingExample/ExcludeGroupExample.java
@@ -0,0 +1,22 @@
+package GroupingExample;
+
+import org.testng.annotations.Test;
+
+public class ExcludeGroupExample {
+	
+	 @Test(groups = { "include-group" })
+	    public void testMethodOne() {
+	        System.out.println("Test method one belonging to include group.");
+	    }
+	 
+	    @Test(groups = { "include-group" })
+	    public void testMethodTwo() {
+	        System.out.println("Test method two belonging to include group.");
+	    }
+	 
+	    @Test(groups = { "include-group", "exclude-group" })
+	    public void testMethodThree() {
+	        System.out.println("Test method three belonging to exclude/include groups.");
+	    }
+
+}
